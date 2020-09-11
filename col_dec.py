d14fa933aa2043233669776edf66552fb2073118
Harsh Bhavsar <49781372+HarshBhavsar023@users.noreply.github.com>
2020-09-12 04:40:31 +0530

    Add files via upload

---
+++ col_dec.py
@@ -0,0 +1,75 @@
+import cv2
+import pandas as pd
+import numpy as np
+import argparse
+
+#Creating argument parser to take image path from command line
+ap = argparse.ArgumentParser()
+ap.add_argument('-i','--image', required=True,help='Image Path')
+args = vars(ap.parse_args())
+img_path = args['image']
+
+#Reading image with openCV
+img = cv2.imread(img_path)
+
+#declaring global variables (are used later on)
+clicked = False
+r = g = b = xpos = ypos = 0
+
+#Reading CSV file with pandas and giving them to each colum
+index = ["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('colors.csv',names=index,header=None)
+
+#creating a draw draw_function
+#It will create rgb values when  we click
+def draw_function(event,x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos,clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+#Calculating distance to get the color name
+#distance formula is
+    # d = abs(Red – ithRedColor) + (Green – ithGreenColor) + (Blue – ithBlueColor)
+def getColourName(R,G,B):
+    minimum=10000
+    for i in range (len(csv)):
+        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"])) + abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimun = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+#setting a mouse callback on a window
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw_function)
+
+#Displaying the images in the Window
+while(1):
+    cv2.imshow("image",img)
+    if(clicked):
+
+
+         #cv2.rectangle(image, startpoint, endpoint, color, thickness) -1 thickness fills rectangle entirely
+         cv2.rectangle(img,(20,20),(750,60),(b,g,r),-1)
+
+         #Creating text string to display ( Color name and RGB values )
+         text = getColourName(r,g,b) + 'R='+ str(r) + 'G='+ str(g) + 'B='+ str(b)
+
+         #cv2.putText(img,text,start,font(0-7), fontScale, color, thickness, lineType, (optional bottomLeft bool) )
+         cv2.putText(img,text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+
+         #For very light colors we will display text in black color
+         if(r+g+b>=600):
+             cv2.putText(img,text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+         clicked = False
+
+    #Breaking the loop when user hits 'ESC' key
+    if cv2.waitKey(20) & 0xFF ==27:
+        break
+cv2.destroyAllWindows()
